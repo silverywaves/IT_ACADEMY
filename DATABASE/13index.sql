840342135e345ac15e73a8b2c1cdb48b227c6593
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-02-03 01:22:14 +0900

    Update 13index.sql

--- DATABASE/13index.sql
+++ DATABASE/13index.sql
@@ -96,4 +96,11 @@ show index from tbl_f;
 -- ----------------------------------------------------------
 -- INDEX 성능 관리
 -- ----------------------------------------------------------
-
+use employees;
+select count(*) from employees.salaries;
+SELECT * FROM employees.salaries;
+SELECT * FROM employees.salaries where to_date = '1986-01-01';
+create index to_date_idx on employees.salaries(to_date);
+show index from employees.salaries;
+alter table employees.salaries drop index to_date_idx;
+SELECT * FROM employees.salaries where to_date = '1986-12-01';

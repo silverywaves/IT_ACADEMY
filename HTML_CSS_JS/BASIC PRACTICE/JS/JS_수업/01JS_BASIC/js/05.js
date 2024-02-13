3c2114bc0a3e19daa0498c4b71448d4aee7921ad
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-02-13 13:50:43 +0900

    Add files via upload

---
+++ HTML_CSS_JS/BASIC PRACTICE/JS/JS_수업/01JS_BASIC/js/05.js
@@ -0,0 +1,7 @@
+
+console.log("05.js start");
+const boxEl = document.querySelector('.box');  
+boxEl.addEventListener('click',function(){   
+    console.log("clicked"); 
+    boxEl.style.backgroundColor='orange'; 
+});

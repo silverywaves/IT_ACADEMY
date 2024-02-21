c0b7dcd14ca622625548ddf091eaa649f4fc18ed
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-02-21 18:09:16 +0900

    Update Hello.java

--- JAVA/ch1/Hello.java
+++ JAVA/ch1/Hello.java
@@ -1,9 +1,12 @@
-package ch1;
+package ch1;	// 패키지 선언
 
-public class Hello {
+// 패키지 가져오기
+import java.util.*;	// 해당 패키지 내 모든 클래스 불러옴
 
-	public static void main(String[] args) {	// main 메서드의 선언문
-		// TODO Auto-generated method stub
+public class Hello {	// 클래스블록
+
+	public static void main(String[] args) {	// main 메서드의 선언문(main 메서드 블록)
+	// public||private||protected static void[리턴자료형/void는 리턴값 없음] 메서드명(입력자료형, 매개변수)
 		System.out.println("Hello, world.");	// 화면에 글자를 출력
 		
 		System.out.println("Hello, /* 이것은 주석 아님*/ world.");

4a232a703bbf194368d5bda0b473913ab4db2a98
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-02-21 09:05:48 +0900

    Add files via upload

---
+++ HTML_CSS_JS/BASIC PRACTICE/JS/JS_수업/05함수/js/script01.js
@@ -0,0 +1,6 @@
+function f1(){
+    console.log('script01_f1 Function call!');
+}
+const f1_01=()=>{
+    console.log('sript01_f1 Function call!')
+};
---
+++ HTML_CSS_JS/BASIC PRACTICE/JS/JS_수업/05함수/js/script02.js
@@ -0,0 +1,6 @@
+function f1(){
+    console.log('script02_f1 Function call!');
+}
+const f1_01=()=>{
+    console.log('sript02_f1 Function call!')
+};

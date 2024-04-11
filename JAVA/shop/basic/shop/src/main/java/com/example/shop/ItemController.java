f0e86495fec9248f68e5824ab1c4fd2b562f1cfd
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-04-11 17:15:41 +0900

    Update ItemController.java

--- JAVA/shop/basic/shop/src/main/java/com/example/shop/ItemController.java
+++ JAVA/shop/basic/shop/src/main/java/com/example/shop/ItemController.java
@@ -41,9 +41,8 @@ public class ItemController {       // public : 다른 폴더에서도 사용 �
         // Item.java 이동 => toString 함수 재정의 or (Lombok 사용시) @ToString 사용
         var a = new Item();
         System.out.println(a.toString());
-//        System.out.println(a.toString());
 //        System.out.println(a.getClass());
-        a.setTitle("abc");  // a.title 에 들어감
+//        a.setTitle("abc");  // a.title 에 들어감
         
         model.addAttribute("name","비싼바지");   // 2. model.addAttribute(전달할 데이터 이름, 데이터) : 데이터가 해당이름으로 .html에 전달됨
         return "list.html";

7cb5ca19c801525861fc13b27d8f5699476e9eb7
silverywaves <155939946+silverywaves@users.noreply.github.com>
2024-04-21 11:58:04 +0900

    Add files via upload

--- JAVA/JSP수업/01_JSP/src/main/java/C03/SimpleDto.java
+++ JAVA/JSP수업/01_JSP/src/main/java/C03/SimpleDto.java
@@ -1,12 +1,13 @@
 package C03;
 
-public class SimpleDto {
+public class SimpleDto {	// SimpleDto 클래스 선언(간단한 데이터 전송 객체(DTO)로 사용)
 
+	// 클래스의 멤버 변수들 선언 => 이름(name), 나이(age), 주소(addr) 정보 저장
 	private String name;
 	private int age;
 	private String addr;
 
-	// toString
+	// toString => 객체의 정보를 문자열로 반환
 	@Override
 	public String toString() {
 		return "SimpleDto [name=" + name + ", age=" + age + ", addr=" + addr + "]";
@@ -37,7 +38,7 @@ public class SimpleDto {
 		this.addr = addr;
 	}
 
-	// 모든 인자 생성자
+	// 모든 인자 생성자(객체를 생성할 때 필요한 모든 정보를 인자로 받아 멤버 변수들 초기화)
 	public SimpleDto(String name, int age, String addr) {
 		super();
 		this.name = name;
